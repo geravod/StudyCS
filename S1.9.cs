ae4699afd2a5b5e61e131658961b3662e4b057ae
geravod <geravod120@gmail.com>
2022-10-29 17:53:51 +0300

    Create S1.9.cs

---
+++ S1.9.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+           int a, b;
+            a = 50;
+            b = 100;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
