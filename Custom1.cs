45662ccfb54ff8da888707c31b5233f1defc28bd
geravod <geravod120@gmail.com>
2022-10-29 17:38:48 +0300

    Create Custom1.cs

---
+++ Custom1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            string name;
+            name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+            string sex;
+            sex = Console.ReadLine();
+            bool withPet;
+            withPet = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine($"{name}, {age}, {sex}, {withPet}");
+        }
+    }
+}
