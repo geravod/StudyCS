05de21ea127469593c35c103208c6d596b136b8d
geravod <geravod120@gmail.com>
2022-10-29 20:26:13 +0300

    Create S2.1.cs

---
+++ S2.1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = 7*x*x + 3*x + 6;
+            Console.WriteLine(y);
+        }
+    }
+}
