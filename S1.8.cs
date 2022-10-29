0f0c537ba25ee87d882ffbb97a29e22a440917c5
geravod <geravod120@gmail.com>
2022-10-29 17:49:17 +0300

    Create S1.8.cs

---
+++ S1.8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            int a, b, c, d;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{a} {b} {c} {d}");
+
+
+
+        }
+    }
+}
