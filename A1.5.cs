50795e57c847546dd518be0c9bbc8d3b1e7b77db
geravod <geravod120@gmail.com>
2022-10-31 00:35:38 +0300

    Create A1.5.cs

---
+++ A1.5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*  С некоторого момента прошло 234 дня. Сколько полных недель прошло за этот период и сколько лет и сколько секунд?*/
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+             int days, weeks, years, sec;
+             days = Convert.ToInt32(Console.ReadLine());
+             weeks = days / 7;
+             years = days / 365;
+             sec = days * 24 * 60 * 60;
+             Console.WriteLine($"{weeks}, {years}, {sec}");
+        }
+    }
+}
