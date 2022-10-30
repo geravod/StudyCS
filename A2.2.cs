d17b79e87abab55daf1df3e9fd191ac01cb88f59
geravod <geravod120@gmail.com>
2022-10-31 00:21:59 +0300

    Create A2.2.cs

---
+++ A2.2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/* Дано двузначное число. Получить число, образованное при перестановке цифр заданного числа.*/
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            int x, a, b;
+            x = Convert.ToInt32(Console.ReadLine());
+            a = x / 10;
+            string c = Convert.ToString(a);
+            b = x % 10;
+            string d = Convert.ToString(b);
+            Console.WriteLine(d + c);
+        }
+    }
+}
