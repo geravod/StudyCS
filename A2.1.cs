f03ee5a16490b34f31efaf4a138c73658af93f15
geravod <geravod120@gmail.com>
2022-10-31 00:12:36 +0300

    Create A2.1.cs

---
+++ A2.1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            /* Дано двузначное число. Найти:
+            а) число десятков в нем;
+            б) число единиц в нем;
+            в) сумму его цифр;
+            г) произведение его цифр. */
+            int a, b, c, d, x;
+            x = Convert.ToInt32(Console.ReadLine());
+            a = x / 10;
+            b = x % 10;
+            c = x % 10 + x / 10;
+            d = x % 10 * (x / 10);
+            Console.WriteLine($"{a}, {b}, {c} ,{d}");
+        }
+    }
+}
