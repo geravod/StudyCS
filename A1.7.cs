8216c4588b3f6a91eb2739d45df2c83ff58bac7c
geravod <geravod120@gmail.com>
2022-10-31 01:05:59 +0300

    Create A1.7.cs

---
+++ A1.7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*  Дан прямоугольник с размерами 543 х 130 мм. Сколько квадратов со стороной 130 мм можно отрезать от него?*/
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+             int a, b, ab;
+             a = Convert.ToInt32(Console.ReadLine());
+             b = Convert.ToInt32(Console.ReadLine());
+             ab = Convert.ToInt32(Console.ReadLine());
+             string c = Convert.ToString(a * b / (ab * ab));
+             Console.WriteLine(c);
+        }
+    }
+}
