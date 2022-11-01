bf6cf544f4c5f3fed5a42d2673afd3d838a18f43
geravod <geravod120@gmail.com>
2022-11-01 21:24:19 +0300

    Create algoritmDlyaOprIntegrala.cs

---
+++ algoritmDlyaOprIntegrala.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/* y = x^{1.5}-5cos2x+e^{-2x} */
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            double a, b, s, height;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+            s = 0;
+            double count = a;
+            double widht = (b - a) / limit;
+            while (count < b)
+            {
+                count += widht;
+                height = Math.Pow(count, 1.5) - 5 * Math.Cos(2 * count) + Math.Exp(-2 * count);
+                s += widht * height;
+            }
+            Console.WriteLine($"решением определенного интеграла является {s}");
+        }
+    }
+}
