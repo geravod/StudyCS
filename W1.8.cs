e7a07a238b081469b2d5ac9481e3272d0fd7fe67
geravod <geravod120@gmail.com>
2022-11-04 00:28:55 +0300

    Create W1.8.cs

---
+++ W1.8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/* W(1.8) Дано число n. Из чисел 1, 4, 9, 16, 25, … напечатать те, которые не превышают n. */
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            int n, a;
+            Console.Write("Введите число n:");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Введите число а:");
+            a = int.Parse(Console.ReadLine());
+            while (n > Math.Pow(a, 2)){
+                Console.WriteLine(Math.Pow(a, 2));
+                a++;
+            }
+        }
+    }
+}
