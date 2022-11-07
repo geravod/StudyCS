4df897bf914c9a7796a4ea752a4bbee4ece2d55a
geravod <geravod120@gmail.com>
2022-11-08 00:55:54 +0300

    Create W2.14.cs

---
+++ W2.14.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/* W2.14. Найти 15 первых натуральных чисел, делящихся нацело на 19 и находящихся в интервале, левая граница которого равна 100. */
+
+namespace Lessons{
+    class Program{
+        static void Main(){
+            int a, q;
+            a = 0;
+            q = 100;
+            while(a < 15){
+                q++;
+                if(q % 19 == 0){
+                    Console.WriteLine(q);
+                    a++;
+                }
+            }
+        }
+    }
+}
